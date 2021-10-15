8400ab48fa35aa090b760cbf65275c1234b4f4b6
Lăng Kiên <kienl2707@gmail.com>
2021-10-15 17:41:30 +0700

    Update bai tap cuoi khoa

--- 04/tailwind.config.js
+++ 04/tailwind.config.js
@@ -5,20 +5,51 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+
+    screens: {
+      'sm': '320px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '375px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '768px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1024px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1110px',
+      // => @media (min-width: 1110px) { ... }
+    },
+
     container: {
       center: true,
+
     },
     extend: {
+
+
+      width:{
+        1200: '1200px'
+      },
+      height:{
+        1200: '1200px'
+      },
 
       fontFamily: {
         body : "'Inter', sans-serif"
       },
 
       colors: {
-        tim: {
-          100: '#8C30F5',
-          200: '#D6B1FF',
+        'brand-purple': {
+          DEFAULT: '#8C30F5',
+          800: '#D6B1FF',
+          100: '#D6B1FF',
         }
+      },
+      fontSize: {
+        'h1': [ "72px", "98px"]
       }
 
   
